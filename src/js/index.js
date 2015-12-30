50e9456d6b2322d27eac345e8dfb815568818106
Arlefreak <arlefreak@gmail.com>
2015-12-30 03:14:01 -0600

    Added insta filter

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,13 @@
         accessToken: '30631706.29293f7.f4b915e4a06c4f63840f6fe094c8ba2c',
         get: 'user',
         userId: 2198850990,
+        // tagName: 'am_a_rte',
+                filter: function(image) {
+            return image.tags.indexOf('am_a_rte') >= 0;
+        },
         resolution: 'standard_resolution',
-        // template: '<li><img src="{{image}}" /></li>',
         template: '<li><a href="{{link}}" target="_blank"><img src="{{image}}" /><p>{{caption}}</a></li>',
-        limit: 10,
+        limit: 50,
         after: function() {
             $('.my-slider').unslider({
                 autoplay: true,
